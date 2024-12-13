380971319e54cb8fe76df5b483ba686fabb95dbf
Jakub Karaś <jakub.karas@onet.pl>
2024-12-13 20:55:29 +0100

    Added rest of classed for first exercise

---
+++ Lab03/DocumentaryBook.cs
@@ -0,0 +1,11 @@
+namespace Lab03
+{
+    internal class DocumentaryBook : Book
+    {
+        public string topic;
+        public DocumentaryBook(string title, Person author, DateTime publishDate, string topic) : base(title, author, publishDate)
+        {
+            this.topic = topic;
+        }
+    }
+}
