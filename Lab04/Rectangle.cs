fa08c488b276c0cb675f6cf605cea4248657c7d9
Jakub Karaś <jakub.karas@onet.pl>
2024-12-15 10:43:23 +0100

    Temporary stuff from Lab04 lessons

---
+++ Lab04/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    internal class Rectangle : Shape
+    {
+        public Rectangle(double x, double y, double height, double width) : base(x, y, height, width)
+        {
+        }
+
+        public override void Draw()
+        {
+            //base.Draw();
+            Console.WriteLine($"prostokat o wspolrzednych x={X}, y+{Y}, o dlugosci {Width} i wysokosci {Height}\n");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-// using Lab01;
-using Lab02;
+//using System.Net.Http.Headers;
+//using System.Transactions;
+//using Lab01;
+//using Lab02;
 // using Lab03;
+using Lab04;
 
 // Lab01
 
@@ -67,39 +70,222 @@ using Lab02;
 // }
 
 
- // Zadanie 4
-// static void Licz()
-// {
-//     Licz licz1 = new Licz(4);
-//     licz1.Dodaj(5);
-//     licz1.Odejmij(3);
-//     licz1.GetValue();
-//
-//     Licz licz2 = new Licz(-6.5);
-//     licz2.Dodaj(5);
-//     licz2.Odejmij(0.33);
-//     licz2.GetValue();
-//
-//     Licz licz3 = new Licz(0);
-//     licz3.Dodaj(3);
-//     licz3.Odejmij(3.01);
-//     licz3.GetValue();
-// }
-// Licz();
+// Zadanie 4
+//static void Licz()
+//{
+//    Licz licz1 = new Licz(4);
+//    licz1.Dodaj(5);
+//    licz1.Odejmij(3);
+//    licz1.GetValue();
+
+//    Licz licz2 = new Licz(-6.5);
+//    licz2.Dodaj(5);
+//    licz2.Odejmij(0.33);
+//    licz2.GetValue();
+
+//    Licz licz3 = new Licz(0);
+//    licz3.Dodaj(3);
+//    licz3.Odejmij(3.01);
+//    licz3.GetValue();
+//}
+//Licz();
 
 // ZADANIE 5
- // static void CreateSumator()
- // {
- //  Sumator sumator = new Sumator([1,2,3,4,5,6]);
- //  Console.WriteLine($"Suma: {sumator.Suma()}");
- //  Console.WriteLine($"Suma podzielnych /2: {sumator.SumaPodziel2()}");
- //  Console.WriteLine($"Elementów: {sumator.IleElementow()}");
- //  sumator.PrintAll();
- //  sumator.PrintIndexRange(1,3);
- //  sumator.PrintIndexRange(-1, 22);
- // }
- // CreateSumator();
- 
- // Lab03
- 
- 
+// static void CreateSumator()
+// {
+//  Sumator sumator = new Sumator([1,2,3,4,5,6]);
+//  Console.WriteLine($"Suma: {sumator.Suma()}");
+//  Console.WriteLine($"Suma podzielnych /2: {sumator.SumaPodziel2()}");
+//  Console.WriteLine($"Elementów: {sumator.IleElementow()}");
+//  sumator.PrintAll();
+//  sumator.PrintIndexRange(1,3);
+//  sumator.PrintIndexRange(-1, 22);
+// }
+// CreateSumator();
+
+// Lab03
+
+
+//class Zwierze {
+//    public virtual void DajGlos() {
+//        Console.WriteLine("dzwiek pies");    
+//    }
+//}
+
+
+//class Pies: Zwierze
+//{
+//    public override void DajGlos()
+//    {
+//        Console.WriteLine("dzwiek pies 2");
+//    }
+//}
+
+//class Kot : Zwierze
+//{
+//    public override void DajGlos()
+//    {
+//        Console.WriteLine("dzwiek kot");
+//    }
+//}
+
+//class Program {
+//    static void Main() {
+//        Zwierze zwierze = new Pies();
+//        Zwierze zwierze1 = new Kot(); 
+//        zwierze.DajGlos();
+//        zwierze1.DajGlos();
+//    }
+//}
+
+//abstract class Figura {
+//    public abstract double ObliczPole();
+
+
+//    public void Info()
+//    {
+//        Console.WriteLine("figura geometryczna");
+//    }
+//}
+
+//class Kwadrat: Figura {
+//    private double bok;
+
+//    public Kwadrat(double bok) { 
+
+//    }
+
+//    public override double ObliczPole()
+//    {
+//        return bok * bok;
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Figura figura = new Kwadrat(5);
+//        figura.Info();
+//        Console.WriteLine("Pole kwadratu:" + figura.ObliczPole());
+//    }
+//}//abstract class Figura {
+//    public abstract double ObliczPole();
+
+
+//    public void Info()
+//    {
+//        Console.WriteLine("figura geometryczna");
+//    }
+//}
+
+//class Kwadrat: Figura {
+//    private double bok;
+
+//    public Kwadrat(double bok) { 
+
+//    }
+
+//    public override double ObliczPole()
+//    {
+//        return bok * bok;
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Figura figura = new Kwadrat(5);
+//        figura.Info();
+//        Console.WriteLine("Pole kwadratu:" + figura.ObliczPole());
+//    }
+//}
+
+//interface IZwierze
+//{
+//    void DajGlos();
+
+//}
+
+//class Pies: IZwierze
+//{
+//    public void DajGlos()
+//    {
+//        Console.WriteLine("Pies wydaje dzwiek");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        IZwierze zwierze = new Pies();
+//        zwierze.DajGlos();
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+       
+//    }
+//}
+
+//List<Shape> shapes =  new List<Shape>();
+
+////
+//shapes.Add(new Rectangle(10, 20, 30, 40));
+//shapes.Add(new Triangle(10, 20, 30, 40));
+////shapes.Add(new Circle(5, 10, 10);
+
+//foreach (Shape shape in shapes)
+//    shape.Draw();
+
+
+//enum Kolor
+//{
+//    Zielony,
+//    Niebieski,
+//    Czerwony = 10,
+//}
+
+class MojeWyjatki : Exception
+{
+    public MojeWyjatki(string message) : base(message)
+    {
+    }
+}
+
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            SprawdzLiczbe(-23);
+        }
+        catch (MojeWyjatki ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        {
+
+        }
+
+        static void SprawdzLiczbe(int liczba)
+        {
+            if (liczba <= 0)
+            {
+                throw new MojeWyjatki("Liczba musi byc wieksza od zera");
+            }
+        }
+    }
+}
+
+//TODO: lab 4 exercise 2
